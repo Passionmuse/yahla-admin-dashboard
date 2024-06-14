a97289c7f6fe1329eeb614625a8786db79007e61
maherasim <asimriazasim107@gmail.com>
2024-06-14 12:48:25 +0500

    admin-yahla dashboard

---
+++ app/Http/Controllers/BannerController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Banner;
+use Illuminate\Http\Request;
+
+class BannerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        
+ 
+        $ringtone  = new Banner();
+        $ringtone->artists_id = $request->artists_id;
+        $ringtone->status = $request->status;
+        $ringtone->audio = $request->audio[0] ?? null;
+        $ringtone->banner = $request->banner ;
+        // if($request->has('ringtone')){
+        //     $path = $request->file('ringtone')->store('/images/ringtone/','public');
+        //     $ringtone->ringtone_path = $path;
+        // }
+        if($ringtone->save()){
+            return redirect()->back()->with('success' , 'Album was successfully created');
+        }else{
+            return redirect()->back()->with('success' , 'Album was not created successfully');
+
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
